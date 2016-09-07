bf4b0c4f10af61036cdf0eae8c7b75df43fb77f5
Sean <powerwaffe@gmail.com>
2016-09-06 21:23:14 -0400

    Modified Calorie Counter program

--- Calorie Counter/Calorie Counter/Form1.Designer.cs
+++ Calorie Counter/Calorie Counter/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.pbBanana.Location = new System.Drawing.Point(16, 15);
             this.pbBanana.Margin = new System.Windows.Forms.Padding(4);
             this.pbBanana.Name = "pbBanana";
-            this.pbBanana.Size = new System.Drawing.Size(171, 193);
+            this.pbBanana.Size = new System.Drawing.Size(129, 157);
             this.pbBanana.TabIndex = 0;
             this.pbBanana.TabStop = false;
             this.pbBanana.Click += new System.EventHandler(this.pbBanana_Click);
@@ -56,10 +56,10 @@
             // pbApple
             // 
             this.pbApple.Image = global::Calorie_Counter.Properties.Resources.AppleCalories;
-            this.pbApple.Location = new System.Drawing.Point(195, 15);
+            this.pbApple.Location = new System.Drawing.Point(153, 15);
             this.pbApple.Margin = new System.Windows.Forms.Padding(4);
             this.pbApple.Name = "pbApple";
-            this.pbApple.Size = new System.Drawing.Size(169, 191);
+            this.pbApple.Size = new System.Drawing.Size(128, 157);
             this.pbApple.TabIndex = 1;
             this.pbApple.TabStop = false;
             this.pbApple.Click += new System.EventHandler(this.pbApple_Click);
@@ -67,10 +67,10 @@
             // pbOrange
             // 
             this.pbOrange.Image = global::Calorie_Counter.Properties.Resources.OrangeCalories;
-            this.pbOrange.Location = new System.Drawing.Point(17, 215);
+            this.pbOrange.Location = new System.Drawing.Point(17, 180);
             this.pbOrange.Margin = new System.Windows.Forms.Padding(4);
             this.pbOrange.Name = "pbOrange";
-            this.pbOrange.Size = new System.Drawing.Size(169, 191);
+            this.pbOrange.Size = new System.Drawing.Size(128, 154);
             this.pbOrange.TabIndex = 2;
             this.pbOrange.TabStop = false;
             this.pbOrange.Click += new System.EventHandler(this.pbOrange_Click);
@@ -78,10 +78,10 @@
             // pbPear
             // 
             this.pbPear.Image = global::Calorie_Counter.Properties.Resources.PearCalories;
-            this.pbPear.Location = new System.Drawing.Point(195, 215);
+            this.pbPear.Location = new System.Drawing.Point(153, 180);
             this.pbPear.Margin = new System.Windows.Forms.Padding(4);
             this.pbPear.Name = "pbPear";
-            this.pbPear.Size = new System.Drawing.Size(169, 192);
+            this.pbPear.Size = new System.Drawing.Size(128, 154);
             this.pbPear.TabIndex = 3;
             this.pbPear.TabStop = false;
             this.pbPear.Click += new System.EventHandler(this.pbPear_Click);
@@ -89,17 +89,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(427, 26);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 26);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.Size = new System.Drawing.Size(109, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Total Calories";
             // 
             // lbCalories
             // 
             this.lbCalories.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lbCalories.Location = new System.Drawing.Point(405, 53);
+            this.lbCalories.Location = new System.Drawing.Point(289, 55);
             this.lbCalories.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbCalories.Name = "lbCalories";
             this.lbCalories.Size = new System.Drawing.Size(133, 28);
@@ -109,7 +110,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(421, 314);
+            this.button1.Location = new System.Drawing.Point(307, 270);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
@@ -120,7 +121,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(421, 350);
+            this.button2.Location = new System.Drawing.Point(307, 306);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
@@ -133,7 +134,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 412);
+            this.ClientSize = new System.Drawing.Size(428, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbCalories);
