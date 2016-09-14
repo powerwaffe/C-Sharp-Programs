46f76342a590a8507a901da6c2729598e35ee6b5
Sean <powerwaffe@gmail.com>
2016-09-14 10:18:21 -0400

    Modified Coin Flip program

--- Tutorial 5-8/Tutorial 5-8/Form1.Designer.cs
+++ Tutorial 5-8/Tutorial 5-8/Form1.Designer.cs
@@ -31,6 +31,14 @@
             this.pbxTails = new System.Windows.Forms.PictureBox();
             this.pbxHeads = new System.Windows.Forms.PictureBox();
             this.btnToss = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxNumOfHeads = new System.Windows.Forms.TextBox();
+            this.tbxPercentOfHeads = new System.Windows.Forms.TextBox();
+            this.tbxNumOfTails = new System.Windows.Forms.TextBox();
+            this.tbxPercentOfTails = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbxTails)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxHeads)).BeginInit();
             this.SuspendLayout();
@@ -38,7 +46,7 @@
             // pbxTails
             // 
             this.pbxTails.Image = global::Tutorial_5_8.Properties.Resources.quarter_tails;
-            this.pbxTails.Location = new System.Drawing.Point(81, 100);
+            this.pbxTails.Location = new System.Drawing.Point(12, 38);
             this.pbxTails.Name = "pbxTails";
             this.pbxTails.Size = new System.Drawing.Size(170, 170);
             this.pbxTails.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -49,7 +57,7 @@
             // pbxHeads
             // 
             this.pbxHeads.Image = global::Tutorial_5_8.Properties.Resources.quarterHeads;
-            this.pbxHeads.Location = new System.Drawing.Point(81, 100);
+            this.pbxHeads.Location = new System.Drawing.Point(12, 38);
             this.pbxHeads.Name = "pbxHeads";
             this.pbxHeads.Size = new System.Drawing.Size(170, 170);
             this.pbxHeads.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -59,7 +67,7 @@
             // 
             // btnToss
             // 
-            this.btnToss.Location = new System.Drawing.Point(130, 48);
+            this.btnToss.Location = new System.Drawing.Point(284, 195);
             this.btnToss.Name = "btnToss";
             this.btnToss.Size = new System.Drawing.Size(75, 23);
             this.btnToss.TabIndex = 2;
@@ -67,11 +75,88 @@
             this.btnToss.UseVisualStyleBackColor = true;
             this.btnToss.Click += new System.EventHandler(this.btnToss_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Num of Heads";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "% of Heads";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Num of Heads";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "% of Tails";
+            // 
+            // tbxNumOfHeads
+            // 
+            this.tbxNumOfHeads.Location = new System.Drawing.Point(269, 35);
+            this.tbxNumOfHeads.Name = "tbxNumOfHeads";
+            this.tbxNumOfHeads.ReadOnly = true;
+            this.tbxNumOfHeads.Size = new System.Drawing.Size(100, 20);
+            this.tbxNumOfHeads.TabIndex = 7;
+            this.tbxNumOfHeads.TextChanged += new System.EventHandler(this.tbxNumOfHeads_TextChanged);
+            // 
+            // tbxPercentOfHeads
+            // 
+            this.tbxPercentOfHeads.Location = new System.Drawing.Point(269, 75);
+            this.tbxPercentOfHeads.Name = "tbxPercentOfHeads";
+            this.tbxPercentOfHeads.ReadOnly = true;
+            this.tbxPercentOfHeads.Size = new System.Drawing.Size(100, 20);
+            this.tbxPercentOfHeads.TabIndex = 8;
+            // 
+            // tbxNumOfTails
+            // 
+            this.tbxNumOfTails.Location = new System.Drawing.Point(269, 110);
+            this.tbxNumOfTails.Name = "tbxNumOfTails";
+            this.tbxNumOfTails.ReadOnly = true;
+            this.tbxNumOfTails.Size = new System.Drawing.Size(100, 20);
+            this.tbxNumOfTails.TabIndex = 9;
+            // 
+            // tbxPercentOfTails
+            // 
+            this.tbxPercentOfTails.Location = new System.Drawing.Point(269, 153);
+            this.tbxPercentOfTails.Name = "tbxPercentOfTails";
+            this.tbxPercentOfTails.ReadOnly = true;
+            this.tbxPercentOfTails.Size = new System.Drawing.Size(100, 20);
+            this.tbxPercentOfTails.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 282);
+            this.ClientSize = new System.Drawing.Size(391, 242);
+            this.Controls.Add(this.tbxPercentOfTails);
+            this.Controls.Add(this.tbxNumOfTails);
+            this.Controls.Add(this.tbxPercentOfHeads);
+            this.Controls.Add(this.tbxNumOfHeads);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnToss);
             this.Controls.Add(this.pbxTails);
             this.Controls.Add(this.pbxHeads);
@@ -80,6 +165,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbxTails)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxHeads)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,6 +174,14 @@
         private System.Windows.Forms.PictureBox pbxHeads;
         private System.Windows.Forms.PictureBox pbxTails;
         private System.Windows.Forms.Button btnToss;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxNumOfHeads;
+        private System.Windows.Forms.TextBox tbxPercentOfHeads;
+        private System.Windows.Forms.TextBox tbxNumOfTails;
+        private System.Windows.Forms.TextBox tbxPercentOfTails;
     }
 }
 
--- Tutorial 5-8/Tutorial 5-8/Form1.cs
+++ Tutorial 5-8/Tutorial 5-8/Form1.cs
@@ -5,6 +5,13 @@ namespace Tutorial_5_8
 {
     public partial class Form1 : Form
     {
+        // Initialize heads and tails counters/percentages
+        int headsCounter = 0;
+        int tailsCounter = 0;
+        double headsPercent = 0;
+        double tailsPercent = 0;
+        int numOfFlips = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,13 +31,36 @@ namespace Tutorial_5_8
                 // Display heads
                 pbxHeads.Visible = true;
                 pbxTails.Visible = false;
+
+                // Increament heads
+                headsCounter++;
+                numOfFlips++;
+                tbxNumOfHeads.Text = headsCounter.ToString();
             }
             else
             {
                 // Display tails
                 pbxHeads.Visible = false;
                 pbxTails.Visible = true;
+
+                // Increment tails
+                tailsCounter++;
+                numOfFlips++;
+                tbxNumOfTails.Text = tailsCounter.ToString();
             }
+            // Calculate percentahes of sides
+            tailsPercent = (1.0 * tailsCounter / numOfFlips) * 100;
+            headsPercent = (1.0 * headsCounter / numOfFlips) * 100;
+
+            // Display 
+            tbxPercentOfHeads.Text = headsPercent.ToString("#.##");
+            tbxPercentOfTails.Text = tailsPercent.ToString("#.##");
+            
+        }
+
+        private void tbxNumOfHeads_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
