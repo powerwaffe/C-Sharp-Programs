583c77f3441676a0c438e5b9aa17799afe3a7242
Sean <powerwaffe@gmail.com>
2016-09-28 10:28:02 -0400

    Added 28Sep 16-2 program.

---
+++ 28Sep 16-2/28Sep 16-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28Sep_16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 89, 78, 93, 94 };
+            int sum = 0;
+            //for (int i = 0; i < grades.Length; i++)
+            //  sum += grades[i];
+            foreach (int grade in grades)
+                sum += grade;
+            Console.WriteLine(sum);
+            Console.WriteLine("Press any key to continue:");
+            Console.ReadKey();
+        }
+    }
+}
